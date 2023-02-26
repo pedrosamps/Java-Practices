7a513aaeb43bb7b4e04faf1d004dc8a7920380ed
pedrosamps <pedrosamps@outlook.com>
2023-02-26 11:59:28 -0300

    Exercise

---
+++ controleRemoto/Main.java
@@ -0,0 +1,10 @@
+package controleRemoto;
+
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
